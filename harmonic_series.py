e8951635bb977b24d77dedf2b05f8fe741e4de2c
brewstermitchell <brewster.mitchell@gmail.com>
2016-05-03 01:34:07 -0700

    added print to file instead of console. only prints occasional information to keep RAM usage low and keep data meaningful
    will not write to file until process completes...

--- harmonic_series.py
+++ harmonic_series.py
@@ -7,20 +7,19 @@ integer_increment = 0
 harmonic = 0
 count_no_drop = 0
 count_drop = 0
-x = 2
 
+with open('log.log', 'a') as file:
+  while harmonic < 80:
+    integer_increment += 1
+    str_integer = str(integer_increment)
+    if '9' in str_integer :
+      count_drop += 1
+    else:
+      count_no_drop += 1
+      harmonic += 1 / integer_increment
+    if integer_increment % 8888888 == 0:
+      file.write('H: {0:40f}  D: {1:40d}  ND: {2:40d}\n'.format(harmonic, count_drop, count_no_drop))
 
-while harmonic < 80:
-  integer_increment += 1
-  str_integer = str(integer_increment)
-  if '9' in str_integer :
-    count_drop += 1
-  else:
-    count_no_drop += 1
-    harmonic += 1 / integer_increment
-  if integer_increment % 8888888 == 0:
-    print('H:', harmonic, 'D:', count_drop, 'ND:', count_no_drop)
-  
 
 print('Final sum: ', harmonic)
 print('Number of terms dropped: ', count_drop)
